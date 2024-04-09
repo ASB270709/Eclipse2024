ed5e98773f4d9affc312e3c844de42b716d4e995
ASB270709 <130920728+ASB270709@users.noreply.github.com>
2024-04-08 22:22:27 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useRef, useEffect} from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
 export default function App() {
+  const moonAnimation = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(moonAnimation, {
+      toValue: 1,
+      duration: 5000, 
+      useNativeDriver: false, 
+    }).start();
+  }, [moonAnimation]);
+
+  const moonLeft = moonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0%', '79.5%']
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={{...styles.moon, left: moonLeft}} /> {}
+      <Text>Eclipse 2024 🌒</Text>
     </View>
   );
 }
@@ -13,8 +27,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000', 
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  moon: {
+    position: 'absolute', 
+    bottom: '50%', 
+    width: 50, 
+    height: 50,
+    borderRadius: 25, 
+    backgroundColor: '#fff', 
   },
 });
